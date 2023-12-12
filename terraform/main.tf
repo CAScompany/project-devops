cc7248f36cda38ea39560637a16520ea95660a3d
charly94630 <carloslopez94630@outlook.com>
2023-12-12 16:39:02 -0300

    removed duplicated code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-# Filter out local zones, which are not currently supported 
-# with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 locals {
   cluster_name = "education-eks-${random_string.suffix.result}"
 }
