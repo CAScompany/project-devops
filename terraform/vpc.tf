2b011299e01d44d36b39144d6c14b44d43a5a893
charly94630 <carloslopez94630@outlook.com>
2023-12-12 17:33:49 -0300

    modified azs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,10 +2,16 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
 
+  providers = {
+    aws = aws.us_east_1
+  }
+
   name = "vpc-ortproyecteks"
 
   cidr = "172.20.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  // Obtén las Availability Zones de us-east-1
+  azs = data.aws_availability_zones.available.names
 
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
@@ -22,4 +28,9 @@ module "vpc" {
     "kubernetes.io/cluster/${var.clusterName}" = "shared"
     "kubernetes.io/role/internal-elb"             = 1
   }
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
 }
