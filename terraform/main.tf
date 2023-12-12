d3f8f46e9fa3bd972a67fd2802afce7be5894802
charly94630 <carloslopez94630@outlook.com>
2023-12-12 14:20:04 -0300

    removed duplicated line

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,11 +9,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-
 
 provider "aws" {
   region = var.region
