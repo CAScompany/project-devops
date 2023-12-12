0062ec0f806052ac0ec9cd34ce6ce90604bdc9d2
scruz1993 <sergitocruz1993@gmail.com>
2023-12-12 23:50:13 +0000

    cambiando subnet

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -7,7 +7,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = "arn:aws:iam::195400145213:role/LabRole"
 
   vpc_config {
-    subnet_ids         = ["subnet-079ce72941a946f2a", "subnet-0b53f4239bbe656d7"]
+    subnet_ids         = ["subnet-079ce72941a946f2a", "subnet-07473c937c3809cc4"]
     security_group_ids = ["sg-06aa9121ecd538fed"]
   }
 
@@ -19,7 +19,7 @@ resource "aws_eks_node_group" "node_group" {
   node_group_name = "eks-cluster-node-group"
   node_role_arn   = "arn:aws:iam::195400145213:role/LabRole"
 
-  subnet_ids = ["subnet-079ce72941a946f2a", "subnet-0b53f4239bbe656d7"]
+  subnet_ids = ["subnet-079ce72941a946f2a", "subnet-07473c937c3809cc4"]
 
   scaling_config {
     desired_size = 1
