00e31782dfa4abe8e3676a584c329b351e6b533e
charly94630 <carloslopez94630@outlook.com>
2023-12-14 05:20:45 -0300

    f fixing syntax error

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -9,13 +9,13 @@ resource "aws_efs_file_system" "stage-clusterEFS" {
 }
 
 resource "aws_efs_mount_target" "efs1" {
-  file_system_id = aws_efs_file_system.clusterEFS.id
+  file_system_id = aws_efs_file_system.stage-clusterEFS.id
   subnet_id      = "subnet-079ce72941a946f2a"  
   security_groups = ["sg-024c332cca1dd7d9a"]
 }
 
 resource "aws_efs_mount_target" "efs2" {
-  file_system_id = aws_efs_file_system.clusterEFS.id
+  file_system_id = aws_efs_file_system.stage-clusterEFS.id
   subnet_id      = "subnet-07473c937c3809cc4"  
   security_groups = ["sg-024c332cca1dd7d9a"]
 }
