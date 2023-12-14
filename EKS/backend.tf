bff3bb46af54d9f6491a859b4966661091652af5
charly94630 <carloslopez94630@outlook.com>
2023-12-14 04:13:30 -0300

    applying dynamodb

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "aws/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
- #   dynamodb_table = "tfstate"
+    dynamodb_table = "tfstate"
   }
 }
